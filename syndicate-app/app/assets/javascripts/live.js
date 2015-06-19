5f296f57f331470d01b812c1c975556ccd9b4bf3
tabdollahi <abdollahi.tania@gmail.com>
2015-06-19 01:03:42 -0700

    refactor ajax calls for vote buttons

--- syndicate-app/app/assets/javascripts/live.js
+++ syndicate-app/app/assets/javascripts/live.js
@@ -10,49 +10,8 @@ $(document).ready(function() {
 
 var listenButtons = function() {
 
-  $(".button").on('click', "#yes-button", function(e) {
-    e.preventDefault();
-    console.log("YEAH BUDDY");
-
-    var issueId = $(".vote-board").attr('id');
-    var url = '/issues/' + issueId + '/vote?value=yes';
-
-    var request = $.ajax({
-      type: "PATCH",
-      url: url
-    });
-
-    request.done(function(data) {
-      console.log("SUCCESS!");
-      console.log(data);
-    });
-
-    request.fail(function(response) {
-      console.log("FAIL!");
-    });
-  });
-
-  $(".button").on('click', "#no-button", function(e){
-    e.preventDefault();
-    console.log("get out.");
-
-    var issueId = $(".vote-board").attr('id');
-    var url = '/issues/' + issueId + '/vote?value=no';
-
-    var request = $.ajax({
-      type: "PATCH",
-      url: url
-    });
-
-    request.done(function(data) {
-      console.log("SUCCESS!");
-      console.log(data);
-    });
-
-    request.fail(function(response) {
-      console.log("FAIL!");
-    });
-  });
+  voteButton("#yes-button", "yes");
+  voteButton("#no-button", "no");
 
 }
 
@@ -68,6 +27,29 @@ var listenParticipants = function() {
     //   $(this).parent().siblings(".delegate-button").hide();
     // })
   }
+
+var voteButton = function(buttonClass, voteValue) {
+  $(".button").on('click', buttonClass, function(e) {
+    e.preventDefault();
+
+    var issueId = $(".vote-board").attr('id');
+    var url = '/issues/' + issueId + '/vote?value='+voteValue;
+
+    var request = $.ajax({
+      type: "PATCH",
+      url: url
+    });
+
+    request.done(function(data) {
+      console.log("SUCCESS!");
+      console.log(data);
+    });
+
+    request.fail(function(response) {
+      console.log("FAIL!");
+    });
+  });
+}
 
 
 
