f0dad96285e69d362a5f92c0947facba1383dfda
Robert Austin <rob@jugglerdigital.com>
2015-06-18 22:11:39 -0700

    delegate sherif

--- syndicate-app/db/seeds.rb
+++ syndicate-app/db/seeds.rb
@@ -5,20 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-50.times do
-  Issue.create(
-      title: Faker::Lorem.sentence,
-      description: Faker::Lorem.paragraph(3),
-      image_url: "/images/sherif.jpg",
-      start_date: Faker::Time.between(2.days.ago, Time.now, :all),
-      finish_date: Faker::Time.forward(23, :all),
-      creator_id: 1,
-      group_id: 1
-  )
+30.times do
+ User.create(username: Faker::Name.first_name)
 end
 
-30.times do
-  User.create(
-    username: Faker::Name.first_name
-  )
+50.times do
+ issue = Issue.create(
+     title: Faker::Lorem.sentence,
+     description: Faker::Lorem.paragraph(3),
+     image_url: "/images/sherif.jpg",
+     start_date: Faker::Time.between(2.days.ago, Time.now, :all),
+     finish_date: Faker::Time.forward(23, :all),
+     creator_id: 1,
+     group_id: 1
+ )
+
+ issue.voters = User.all
 end
