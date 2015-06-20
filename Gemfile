48125a828396c08a1cfa7ab554b4feefb1a16fb7
Robert Austin <rob@jugglerdigital.com>
2015-06-20 15:39:18 -0700

    firebase test

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 # gem 'therubyracer', group: :production
 gem 'ancestry'
-
+gem 'firebase'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
 # Use postgresql as the database for Active Record
--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -6,50 +6,33 @@ class IssuesController < ApplicationController
   def show
     @current_issue = Issue.find(params[:id])
     p @current_issue
-  end
 
-  def vote
-    # p "Got here!"
-    # p params
-    @vote = current_user.votes.find_by(issue_id: params[:id])
 
-    if @vote.root?
-      @vote.update_attributes(value: params[:value])
-      @vote.save
-    else
-      puts "User has delegated their vote."
-    end
-
-    @vote.descendants.each do |vote|
-      vote.value = @vote.value
-      vote.save
-    end
-
-    render json: @vote
   end
 
   def delegate
     @representative = User.find(params[:id])
     @representative_vote = @representative.votes.find_by(issue_id: params[:issue_id])
-
     @current_user_vote = current_user.votes.find_by(issue_id: params[:issue_id])
-    # @old_parent_vote = @current_user_vote.parent
-    # p '*'*80
-    # p @current_user_vote
-    # p @old_parent_vote
-    # p '*'*80
-    if @current_user_vote.parent != nil
-      @old_parent_vote = @current_user_vote.parent
-      @old_parent = User.find(@old_parent_vote.user_id)
-      # p '*'*80
-      # p @old_parent.get_vote_power(params[:issue_id])
-      # p '*'*80
-    end
+
+
+
     @current_user_vote.parent = @representative_vote
     @current_user_vote.save
 
+    p @representative_vote.descendants
 
-      #render json of two objects, both the
+    base_uri = 'https://incandescent-heat-2238.firebaseio.com/'
+
+    firebase = Firebase::Client.new(base_uri)
+
+    response = firebase.push("todos", { :name => 'Pick the milk', :priority => 1 })
+    response.success? # => true
+    response.code # => 200
+    response.body # => { 'name' => "-INOQPH-aV_psbk3ZXEX" }
+    response.raw_body # => '{"name":"-INOQPH-aV_psbk3ZXEX"}'
+
+    #render json of two objects, both the
 
     # p @representative_vote.descendants
     # @delegate_vote.descendants.each do |vote|
@@ -57,7 +40,7 @@ class IssuesController < ApplicationController
     #   vote.save
     # end
 
-    render json: @representative.get_vote_power(params[:issue_id])
+    render json: response.body
 
   end
 
