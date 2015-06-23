fe92f5559616af81261f7d7134a133c2698b0ba6
Robert Austin <rob@jugglerdigital.com>
2015-06-22 20:19:14 -0700

    vote status effects, colour chart, leaderboard mobile styling, delegate bug fixes

--- app/assets/javascripts/live.js
+++ app/assets/javascripts/live.js
@@ -33,6 +33,7 @@ $(document).on("ready, page:change", function() {
         appendVoteStatus();
         appendDelegatedStatus(message.current_user_id);
         nestParticipant(message.current_user_id, message.new_rep_id)
+
       }
       else if (message.incident === "undelegate") {
         appendScore(message.old_delegate_count, message.old_delegate_id);
@@ -54,7 +55,9 @@ $(document).on("ready, page:change", function() {
         message.yes_percentage,
         message.no_percentage,
         message.vote_count,
-        message.abstain_count
+        message.abstain_count,
+        message.current_user_id,
+        message.current_user_vote_value
       );
     });
   }
@@ -70,7 +73,7 @@ var voteButton = function(buttonClass, voteValue) {
   $(".vote-button").on('click', buttonClass, function(e) {
     e.preventDefault();
 
-    var issueId = $(".leaderboard").attr('id').slice(6);
+    var issueId = $(".issues").attr('id').slice(6);
     var url = '/issues/' + issueId + '/vote?value=' + voteValue;
 
     var request = $.ajax({
@@ -91,7 +94,7 @@ var voteButton = function(buttonClass, voteValue) {
   });
 }
 
-var changeVoteDOM = function(participantCount, yesVotes, noVotes, yesPercentage, noPercentage, voteCount, abstainCount) {
+var changeVoteDOM = function(participantCount, yesVotes, noVotes, yesPercentage, noPercentage, voteCount, abstainCount, currentUser, currentUserVoteValue ) {
   $('#total-participants').html(participantCount);
   $('#yes-votes').html(yesVotes);
   $('#no-votes').html(noVotes);
@@ -103,6 +106,9 @@ var changeVoteDOM = function(participantCount, yesVotes, noVotes, yesPercentage,
   myDoughnutChart.segments[0].value = noVotes;
   myDoughnutChart.segments[1].value = yesVotes;
   myDoughnutChart.update();
+
+  appendVoteStatus(currentUser, currentUserVoteValue)
+  appendVoteZone(currentUser, currentUserVoteValue)
 }
 
 var delegateButton = function(){
@@ -115,7 +121,7 @@ var delegateButton = function(){
     console.log(array)
     // When we delegate our vote by clicking on another user they are our "representative"
     var representative = $(this)
-    var issueId = $(".leaderboard").attr('id').slice(6);
+    var issueId = $(".issues").attr('id').slice(6);
     console.log(issueId)
     var representativeId = $(this).attr('id');
     var url = '/issues/' + issueId + '/users/' + representativeId + '/delegate';
@@ -154,13 +160,24 @@ var nestParticipant = function(current_user_id, new_rep_id) {
   $('#' + new_rep_id).children(".constituents").append(constituentDomTemplate)
 };
 
+var appendVoteZone = function(current_user, currentUserVoteValue) {
+  // Moves the delegate under the representative in the dom
+  var constituentDomTemplate = $('#' + current_user)
+  $('.zone-yes').append(constituentDomTemplate)
+};
+
+
 var unnestParticipant = function(current_user_id, new_rep_id) {
   var constituentDomTemplate = $('#' + current_user_id)
   $(".participants").append(constituentDomTemplate)
 };
 
-var appendVoteStatus = function() {
-
+var appendVoteStatus = function(current_user, currentUserVoteValue) {
+  console.log("Append Vote " + currentUserVoteValue)
+  $('#' + current_user).children().children(".badge").removeClass("abstain")
+  $('#' + current_user).children().children(".badge").removeClass("yes")
+  $('#' + current_user).children().children(".badge").removeClass("no")
+  $('#' + current_user).children().children(".badge").addClass(currentUserVoteValue)
 }
 
 var appendDelegatedStatus = function(current_user) {
@@ -182,14 +199,14 @@ var newDrawChart = function(yes_votes, no_votes) {
   var data = [
     {
       value: no_votes,
-      color:"#F7464A",
-      highlight: "#FF5A5E",
+      color:"#F5781E",
+      highlight: "#ff9042",
       label: "Red"
     },
     {
       value: yes_votes,
-      color: "#46BFBD",
-      highlight: "#5AD3D1",
+      color: "#3abc95",
+      highlight: "#3bcea2",
       label: "Green"
     }
   ];
